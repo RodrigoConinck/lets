6a5d1529cf50f464b176ad9b94deb75951a18b6b
RodrigoConinck <rodrigo.coninck@hotmail.com>
2023-06-18 13:43:51 -0300

    update

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 var cors = require('cors')
-const registrar = require('./service/user/register')
-const update = require('./service/user/update')
-const login = require('./service/user/login/login')
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 require('dotenv').config()
 
@@ -16,26 +14,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.post('/register', async function (req, res) {
-  const {nome, email, senha} = req.body;
-  console.log(nome,email,senha);
-  const newRegister = await registrar(nome, email, senha)
-  res.send(newRegister)
-});
-
-app.post('/login', async function(req, res){
-  const {email, password} = req.body;
-  const token = await login(email, password)
-  res.send({token:token})
-})
-
-app.post('/perfil', async function (req, res) {
-  const {nome, idade, sexo, altura, peso, estado, cidade, bairro, atividades_preferenciais, frequencia, ativo_fisicamente, foto_perfil} = req.body;
-  console.log(nome, idade, sexo, altura, peso, estado, cidade, bairro, atividades_preferenciais, frequencia, ativo_fisicamente, foto_perfil);
-  const newRegister = await update(nome, idade, sexo, altura, peso, estado, cidade, bairro, atividades_preferenciais, frequencia, ativo_fisicamente, foto_perfil)
-  res.send(newRegister)
-});
-
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
