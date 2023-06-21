60df94b8c233009e5a38d9e894cf860fd87ea430
RodrigoConinck <rodrigo.coninck@hotmail.com>
2023-06-20 22:58:36 -0300

    criação dos testes unitários

---
+++ backend/__tests__/likes/create.test.js
@@ -0,0 +1,112 @@
+const controller = require('../../controllers/likes/create')
+const Likes = require('../../models/likes')
+const Matchs = require('../../models/matchs')
+const { Op } = require('sequelize')
+
+jest.mock('../../models/likes', () => ({
+  create: jest.fn(),
+  findOne: jest.fn(),
+}))
+
+jest.mock('../../models/matchs', () => ({
+  create: jest.fn(),
+}))
+
+describe('Your Controller', () => {
+  beforeEach(() => {
+    Likes.create.mockClear()
+    Likes.findOne.mockClear()
+    Matchs.create.mockClear()
+  })
+
+  test('should create a new like and check for a match', async () => {
+    const request = {
+      body: {
+        curtidor: 1,
+        curtido: 2,
+      },
+    }
+
+    const expectedLike = { fk_curtidor: 1, fk_curtido: 2 }
+    Likes.create.mockResolvedValue(expectedLike)
+
+    const expectedMatch = {
+      fk_primeiro_usuario: 2,
+      fk_segundo_usuario: 1,
+    }
+    Likes.findOne.mockResolvedValue({ fk_curtidor: 2, fk_curtido: 1 })
+    Matchs.create.mockResolvedValue(expectedMatch)
+
+    const res = {
+      send: jest.fn(),
+    }
+
+    await controller(request, res)
+
+    expect(Likes.create).toHaveBeenCalledWith(expectedLike)
+  })
+
+  test('should create a new like without a match', async () => {
+    const request = {
+      body: {
+        curtidor: 1,
+        curtido: 2,
+      },
+    }
+
+    const expectedLike = { fk_curtidor: 1, fk_curtido: 2 }
+    Likes.create.mockResolvedValue(expectedLike)
+
+    Likes.findOne.mockResolvedValue(null) // No match found
+
+    const res = {
+      send: jest.fn(),
+    }
+
+    await controller(request, res)
+
+    expect(Likes.create).toHaveBeenCalledWith(expectedLike)
+    expect(Matchs.create).not.toHaveBeenCalled()
+  })
+
+  test('should handle error and send error message', async () => {
+    const request = {
+      body: {
+        curtidor: 1,
+        curtido: 2,
+      },
+    }
+
+    const expectedErrorMessage = 'Error creating like'
+    Likes.create.mockRejectedValue(new Error(expectedErrorMessage))
+
+    const res = {
+      send: jest.fn(),
+    }
+
+    await controller(request, res)
+
+    expect(Likes.create).toHaveBeenCalled()
+    expect(Likes.findOne).not.toHaveBeenCalled()
+    expect(Matchs.create).not.toHaveBeenCalled()
+  })
+
+  test('should throw error if required fields are missing', async () => {
+    const request = {
+      body: {
+        curtidor: 1,
+      },
+    }
+
+    const res = {
+      send: jest.fn(),
+    }
+
+    await controller(request, res)
+
+    expect(Likes.create).not.toHaveBeenCalled()
+    expect(Likes.findOne).not.toHaveBeenCalled()
+    expect(Matchs.create).not.toHaveBeenCalled()
+    expect(res.send).toHaveBeenCalledWith({ message: 'Campos obrigatórios' })
+  })
+})
---
+++ backend/__tests__/users/delete.test.js
@@ -0,0 +1,52 @@
+const controller = require('../../controllers/users/delete')
+const Usuario = require('../../models/usuario')
+
+jest.mock('../../models/usuario', () => ({
+  destroy: jest.fn(),
+}))
+
+describe('Your Controller', () => {
+  beforeEach(() => {
+    Usuario.destroy.mockClear()
+  })
+
+  test('should destroy a user and return the number of affected rows', async () => {
+    const request = {
+      query: {
+        id: 1,
+      },
+    }
+
+    const expectedRowsAffected = 1
+    Usuario.destroy.mockResolvedValue(expectedRowsAffected)
+
+    const res = {
+      send: jest.fn(),
+    }
+
+    await controller(request, res)
+
+    expect(Usuario.destroy).toHaveBeenCalledWith({
+      where: {
+        id: request.query.id,
+      },
+    })
+    expect(res.send).toHaveBeenCalledWith({ rows: expectedRowsAffected })
+  })
+
+  test('should handle error and send error message', async () => {
+    const request = {
+      query: {},
+    }
+
+    const expectedErrorMessage = 'id do usuario é obrigatorio!'
+    const res = {
+      send: jest.fn(),
+    }
+
+    await controller(request, res)
+
+    expect(Usuario.destroy).not.toHaveBeenCalled()
+    expect(res.send).toHaveBeenCalledWith(expectedErrorMessage)
+  })
+})
